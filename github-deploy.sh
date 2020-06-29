ab1f03a3fd4a800efc40c2a1b0f5ee7e71aa8b7d
Jithin Isaac <jithinsaji@gmail.com>
2020-06-29 20:33:17 +0530

    Main Hugo project files commit Mon, Jun 29, 2020  8:33:16 PM

--- github-deploy.sh
+++ github-deploy.sh
@@ -2,21 +2,26 @@
 	
 	# If a command fails then the deploy stops
 	set -e
-	printf "\033[0;32mDeploying updates to website main repository...\033[0m\n"
-	
+
+	#COMMITING & PUBLISHING TO Portfolio MAIN PROJECT REPOSITORY
+	printf "\033[0;32mDeploying updates to Portfolio main project repository...\033[0m\n"
+	# Add changes to git.
 	git add .
 	
-	msg="rebuilding site $(date)"
-	
+	# Commit changes.
+	msg="Main Hugo project files commit $(date)"	
 	if [ -n "$*" ]; then
 		msg="$*"
 	fi
-	
 	git commit -m "$msg"
 	
+	# Push source and build repos.
 	git push origin master
 
-	printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+
+	#COMMITING & PUBLISHING TO Portfolio MAIN Website REPOSITORY
+	printf "\033[0;32mDeploying updates to Portfolio Website...\033[0m\n"
 	# Build the project.
 	hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 	
@@ -27,7 +32,8 @@
 	git add .
 	
 	# Commit changes.
-	msg="rebuilding site $(date)"
+	msg="Rebuilding site $(date)"
+
 	if [ -n "$*" ]; then
 		msg="$*"
 	fi
